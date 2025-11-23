4b7cbd6a0bff11a14a200f107eabf575d78ec611
Michael Candia <michael.candia.112@gmail.com>
2025-11-22 21:20:23 -0300

    Security Group para EC2 y permitimos el trafico HTTP en el puerto 80

--- Ejercicio_2/Ej_2.py
+++ Ejercicio_2/Ej_2.py
@@ -1,10 +1,11 @@
 import boto3
 from botocore.exceptions import ClientError
 
+ec2 = boto3.client('ec2')
 s3 = boto3.client('s3')
 
 bucket_name = 'obligatorio-devops-boto3'
-file_path = 'obligatorio.zip'
+file_path = 'obligatorio-main.zip'
 object_name = file_path.split('/')[-1]
 
 try:
@@ -26,3 +27,43 @@ except FileNotFoundError:
 except ClientError as e:
     print(f"Error subiendo archivo al bucket: {e}")
     exit(1)
+
+#Security Group para ec2
+
+sg_name ='ec2-web-sg'
+sg_desc ='Security group para la web'
+
+resp = ec2.describe_security_groups(
+    Filters=[{'Name':'group-name','Values': [sg_name]}]
+)
+if resp['SecurityGroups']:
+    ec2_sg_id=resp['SecurityGroups'][0]['GroupId']
+    print(f"El Security Group {ec2_sg_id} ya existe, lo reutilizare")
+else:
+    ec2_sg_id = ec2.create_security_group(
+        GroupName='ec2-web-sg',
+        Description='Security group para la web'
+    )
+    print(f"Security Group de EC2 creado con exito: {ec2_sg_id}")
+
+#Permitimos el trafico de HTTP(Puerto 80) para la app
+
+try:
+    ec2.authorize_security_group_ingress(
+        GroupId=ec2_sg_id,
+        IpPermissions=[
+            {
+                'IpProtocol':'tcp',
+                'FromPort':80,
+                'ToPort':80,
+                'IpRanges':[{'CidrIp': '0.0.0.0/0'}]
+            }
+        ]
+    )
+except ClientError as e:
+    if e.response['Error']['Code'] == 'InvalidPermission.Duplicate' :
+        print(f"La regla ya existe en el Security Group, la voy a reutilizar")
+    else:
+        raise
+
+

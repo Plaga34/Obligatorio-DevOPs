a1e2406b49bed71f992f54aa4582180e641d6866
Inti Lopez <intilopez97@gmail.com>
2025-11-24 00:58:39 +0000

    Nuevos verificadores para que el archivo no de error al entrar vacio,no permiso de lectura, que sea un archivo y dejamos preparado para el recorrido

--- Ejercicio_1/Ej_1.sh
+++ Ejercicio_1/Ej_1.sh
@@ -8,13 +8,12 @@ IFS="
 
 if [ $# -gt 4 ];then
 	echo Solo se admiten 4 parametros -i -c seguido de la contraseña y el archivo
-	exit 6 #Esto hay que ordenarlo, por experiencia al final
+	exit 1 #Esto hay que ordenarlo, por experiencia al final
 fi
 #Menos de dos paramentros aca no che
 if [ $# -lt 2 ];then
-	echo Falta gente en el cuadro
 	echo Como minimo se admite el archivo como parametro
-	exit 7
+	exit 2
 fi
 
 #Creamos un while para recorrer los parametros
@@ -30,13 +29,39 @@ while [ $# -gt 1 ];do	#Recorremos los parametros
 				exit 2
 			fi
 			contra="$2"	#Guardo el -p y la contraseña para tirar la variable entera al useradd
-			shift 2
+			shift 3
 			;;
 		-*)	#COMENTAR
 			echo "Error, el parametro $1 no es valido" >&2
-			exit 3
+			exit 4
 			;;
 	esac
 done
 
 archivo="$1"	#Descarte todo, solo me queda el archivo lo guardo
+#Archivo ingresado igual a vacio
+if [ -z "$archivo" ];then
+        echo "Error, no ingreso ningun archivo o " >&2
+        exit 5
+fi
+#El archivo no es un archivo.. si pasa este exit tambien existe
+if ! [ -f "$archivo" ];then
+        echo Esto no es un archivo
+        exit 6
+fi
+#Permiso de lectura sobre el archivo?
+if ! [ -r "$archivo" ];then
+        echo No tiene permisos de lectura sobre el archivo
+        exit 7
+fi
+cont=0
+#reviso el archivo linea a linea si es correcto
+for linea in $(cat $archivo);do
+         cont=$((cont+1))
+        if ! [[ "$linea" =~ ^[a-z_][a-z0-9_-]*:[^:]*:.*:(SI|NO):.*$ ]];then
+                #Campo usuario no vacio y que se respeten los espacios
+                echo Estructura del archivo incorrecta, revise la linea $cont
+                exit 8
+        fi
+done
+

5fe61569e4a3e32463d65ff420ae19985cf306b0
Inti López <intilopez97@gmail.com>
2025-11-27 09:01:52 -0300

    Agrego lo pensado previamente que no cargue anoche, aca tenemos el famoso UserData

--- Ejercicio_2/Ej_2.py
+++ Ejercicio_2/Ej_2.py
@@ -104,4 +104,181 @@ except ClientError as e:
     else:
         raise
 
+# Creamos RDS con sus parámetros
+
+DB_INSTANCE_ID = 'app-mysql'
+DB_NAME = 'demo_db'
+DB_USER = 'admin'
+
+#Solicitamos la password a traves de un input
+DB_PASS = getpass("Introduce la contraseña del admin RDS: ")
+
+if not DB_PASS:
+    raise Exception('Por favor ingrese una contraseña valida.')
+
+try:
+    rds.create_db_instance(
+        DBInstanceIdentifier=DB_INSTANCE_ID,
+        AllocatedStorage=20,
+        DBInstanceClass='db.t3.micro',
+        Engine='mysql',
+        MasterUsername=DB_USER,
+        MasterUserPassword=DB_PASS,
+        DBName=DB_NAME,
+        PubliclyAccessible=True,
+        BackupRetentionPeriod=0,
+        VpcSecurityGroupIds=[rds_sg_id]
+    )
+    print(f"Instancia RDS {DB_INSTANCE_ID} creada correctamente.")
+
+except rds.exceptions.DBInstanceAlreadyExistsFault:
+
+    print(f"La instancia {DB_INSTANCE_ID} ya existe.")
+
+#Colocamos un mensaje en pantalla mientras la instancia inicie
+print(f"Espere mientras se crea la instancia")
+
+#esperamos a que la instancia este creada
+waiter = rds.get_waiter('db_instance_available')
+waiter.wait(DBInstanceIdentifier=DB_INSTANCE_ID)
+
+#Sacamos la informacion de la instancia
+resp_db = rds.describe_db_instances(DBInstanceIdentifier=DB_INSTANCE_ID)
+rds_endpoint = resp_db ['DBInstances'][0]['Endpoint']['Address']
+
+
+user_data = f'''#!/bin/bash
+dnf update -y
+dnf install -y httpd unzip awscli php php-cli php-fpm php-common php-mysqlnd mariadb105 -y
+systemctl start httpd
+systemctl enable httpd
+
+#Creamos la carpeta donde se va a alojar la aplicacion y nos posicionamos en ella
+mkdir -p /var/www/html/app
+cd /var/www/html/app
+
+#Descargamos el zip desde la instancia S3
+#aws s3 cp s3://{bucket_name}/{object_name} /var/www/html/app/obligatorio.zip
+aws s3 cp s3://{bucket_name}/{object_name} /tmp/obligatorio.zip
+
+#Descomprimimos el archivo de la aplicacion
+#unzip -o /var/www/html/app/obligatorio.zip -d /var/www/html/app/obligatorio-main
+unzip -o /tmp/obligatorio.zip -d /tmp/
+
+cp /tmp/obligatorio-main/app.css /var/www/html/app.css
+cp /tmp/obligatorio-main/app.js /var/www/html/app.js
+cp /tmp/obligatorio-main/config.php /var/www/html/config.php
+cp /tmp/obligatorio-main/index.html /var/www/html/index.html
+cp /tmp/obligatorio-main/index.php /var/www/html/index.php
+cp /tmp/obligatorio-main/login.css /var/www/html/login.css
+cp /tmp/obligatorio-main/login.html /var/www/html/login.html
+cp /tmp/obligatorio-main/login.js /var/www/html/login.js
+cp /tmp/obligatorio-main/login.php /var/www/html/login.php
+cp /tmp/obligatorio-main/init_db.sql /var/www/init_db.sql
+
+
+mysql -h {rds_endpoint} -u {DB_USER} -p{DB_PASS} {DB_NAME} < /var/www/init_db.sql
+   
+sudo tee /var/www/.env >/dev/null <<'ENV'
+DB_HOST={rds_endpoint}
+DB_NAME={DB_NAME}
+DB_USER={DB_USER}
+DB_PASS={DB_PASS}
+
+ENV
+
+sudo chown -R apache:apache /var/www/.env
+sudo chmod -R 600 /var/www/.env```
+
+sudo chown -R apache:apache /var/www/html
+sudo chmod -R 755 /var/www/html
+
+#echo "La aplicacion se desplego correctamente" > /var/www/html/index.html
+
+#Reiniciamos apache
+systemctl restart httpd php-fpm
+'''
+
+# Lanzamos la instancia EC2 con el bash anterior
+ec2_response = ec2.run_instances(
+    ImageId='ami-06b21ccaeff8cd686',
+    InstanceType='t2.micro',
+    MinCount=1,
+    MaxCount=1,
+    IamInstanceProfile={'Name': 'LabInstanceProfile'},
+    SecurityGroupIds=[ec2_sg_id],
+    UserData=user_data
+
+)
+
+# Obtenemos el ID de la instancia creada
+ec2_id = ec2_response['Instances'][0]['InstanceId']
+
+#Creamos un TAG
+ec2.create_tags(
+    Resources=[ec2_id],
+    Tags=[{'Key': 'Name', 'Value': 'ec2-web'}]
+)
+print(f"Instancia creada con ID: {ec2_id} y nombre de instancia 'ec2-web'")
+
+#Esperamos que la instancia este corriendo
+ec2.get_waiter('instance_status_ok').wait(InstanceIds=[ec2_id])
+
+
+
+
+#'Endpoint': {
+#            'Address': 'string',
+
+
+
+#dejar donde tienen que ir y corregir nombres de variables
+
+#Comandos para instalar app en repo
+
+#EC2
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html
+
+#Crear Instancias EC2
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/run_instances.html
+
+#Crear Tags
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/create_tags.html
+
+#Waiter para EC2
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/waiter/InstanceStatusOk.html
+
+#-----------------------------------------------------------------------------------------------
+
+#S3
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
+
+#Crear Bucket S3
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/create_bucket.html
+
+#Subir archivos S3
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/upload_file.html
+
+
+#-----------------------------------------------------------------------------------------------
+
+#RDS
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds.html
+
+#Creamos instancia de RDS
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds/client/create_db_instance.html
+
+
+
+#Documentacion de Security Group
+#https://boto3.amazonaws.com/v1/documentation/api/latest/guide/ec2-example-security-group.html
+
+#Crear Security Groups
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/create_security_group.html
+
+#Autorizar Security Groups
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/authorize_security_group_ingress.html
+
+#Instancias EC2
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/run_instances.html
 
